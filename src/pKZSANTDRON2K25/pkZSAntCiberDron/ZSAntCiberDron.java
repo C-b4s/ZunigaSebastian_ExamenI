89c80c944382250514b8e67e9a7a8d0a61ebb744
C-b4s <szuniga046@gmail.com>
2025-11-27 00:04:08 -0500

    Se realizaron correcciones a las clases ZSAntCiberDron y ZSAppCiberDron

--- src/pKZSANTDRON2K25/pkZSAntCiberDron/ZSAntCiberDron.java
+++ src/pKZSANTDRON2K25/pkZSAntCiberDron/ZSAntCiberDron.java
@@ -1,5 +1,29 @@
 package pKZSANTDRON2K25.pkZSAntCiberDron;
 
+import java.util.Scanner;
+
+import pKZSANTDRON2K25.pkZSAutomataBBA.ZSAutomataBBA;
+
 public class ZSAntCiberDron {
 
+    public Boolean zsCrearBombaBBA(Scanner sc){
+        ZSAutomataBBA automata = new ZSAutomataBBA();
+         System.out.println("\n[*] AUTÓMATA para DESTRUIR ARSENAL BELICO ================");
+         System.out.println("Coordenadas a destruir: 4 y 2\n");
+        System.out.println("Ingrese el tipo de arsenal para validar (escriba 'salir' para terminar):\n");
+
+            System.out.print("Arsenal belico: ");
+            String cadena = sc.nextLine().trim();
+
+
+            boolean resultado = automata.zsEvaluar(cadena);
+
+            if (resultado) {
+                System.out.println("    Confirmado. Arsenal belico identificado exitosamentet\n");
+                return true;
+            } else {
+                System.out.println("     ERROR. El arsenal belico no ha sido identificado\n");
+                return false;
+            }
+    }
 }
